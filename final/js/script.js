3fdca9ebd00c5a45e7de5d37297c018a16c2efcd
xanthfan <changjoe01@gmail.com>
2013-09-12 16:06:29 +0800

    Final

---
+++ final/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $("#About").click(function(){
+    alert("Text: " + $("#test").text());
+  });
+   $("#New").click(function(){
+    alert("Text: " + $("#test").text());
+  });
+   $("#Press").click(function(){
+    alert("Text: " + $("#test").text());
+  });
+   $("#Follow").click(function(){
+    alert("Text: " + $("#test").text());
+  });
+   $("#Newsletter").click(function(){
+    alert("Text: " + $("#test").text());
+  });
+   $("#Contact").click(function(){
+    alert("Text: " + $("#test").text());
+  });
+});
