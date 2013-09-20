bc24afa69011cf2a10ce89005feea271fdc980a3
xanthfan <changjoe01@gmail.com>
2013-09-20 15:53:05 +0800

    final

--- final/js/script.js
+++ final/js/script.js
@@ -1,27 +1,39 @@
 $(document).ready(function(){
+
   $("#About").click(function(){
     $("#center_content").empty();
     var code=$("<img class =\"changingContent\" src=\"images/sea.jpg\">"); 
     $("#center_content").append(code); 
   });
+
    $("#buttonMap").click(function(){
     $("#center_content").empty();
       var centerWidth = $("#center_content").attr("width");
       var centerHeight = $("#center_content").attr("height");
       var code=$("<iframe class=\"changingContent\" src=\"mapTest.html\" width=centerWidth height=centerHeight frameborder=\"0\"></iframe>"); 
-
     $("#center_content").append(code); 
   });
+
    $("#Press").click(function(){
     alert("Text: " + $("#test").text());
   });
+
    $("#Follow").click(function(){
     alert("Text: " + $("#test").text());
   });
+
    $("#Newsletter").click(function(){
     alert("Text: " + $("#test").text());
   });
+
    $("#Contact").click(function(){
     alert("Text: " + $("#test").text());
   });
+
+  $.getJSON("http://xanthfan.github.io/FEWD/final/data/sample.json",function(result){
+      $.each(result, function(i, field){
+        $("section_3").append(field + " ");
+      });
+    });
+     
 });
