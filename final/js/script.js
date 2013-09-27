dd849f275053ef6d26098a9b8cc77af75cac8932
xanthfan <changjoe01@gmail.com>
2013-09-27 21:00:16 +0800

    post-final

--- final/js/script.js
+++ final/js/script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+  var latHK = 22.29;
+  var lngHK = 114.18;
+
   $("#Login").click(function(){
     $("#center_content").empty();
     var code=$("<iframe class=\"changingContent\" src=\"login.html\" width=centerWidth height=centerHeight frameborder=\"0\"></iframe>"); 
